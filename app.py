e77f919d71ad44dae7c5d20d534280e3fbe0c8d9
codybaraks <earvinbaraka@gmail.com>
2019-01-11 15:20:35 +0300

    cms and Design layou

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request, flash
 import mysql.connector as connector
 from validation import *
 
-db = connector.connect(host="localhost", user="root", passwd="root", database="Dennis")
+db = connector.connect(host="localhost", user="root", passwd="root", database="dennis")
 
 app = Flask(__name__)
 app.secret_key = "fsggrsgsrgrg"
